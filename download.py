955a3eff381cac8e1c2811f2bfde3832e68c576e
orange2008 <40315138+orange2008@users.noreply.github.com>
2021-06-28 12:48:09 +0800

    Added gitignore and fixed bugs that can't download to the same folder

--- download.py
+++ download.py
@@ -2,6 +2,9 @@ from urllib.request import urlretrieve
 import os
 def download(url, cot, ver, filename):
     print(url + " => " + str(filename))
-    os.mkdir("/disk/CTD/" + cot.title() + "-" + ver)
+    try:
+        os.mkdir("/disk/CTD/" + cot.title() + "-" + ver)
+    except FileExistsError:
+        pass
     urlretrieve(url, "/disk/CTD/" + cot.title() + "-" + ver + "/" + str(filename))
     return True
